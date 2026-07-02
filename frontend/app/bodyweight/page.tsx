5af3e454470f2965db5ba196b44b4a469878b05e
elian <elianpullen@protonmail.com>
2026-07-02 11:51:58 +0200

    style(frontend): add table to bodyweight page

--- frontend/app/bodyweight/page.tsx
+++ frontend/app/bodyweight/page.tsx
@@ -1,18 +1,36 @@
-import { getBodyWeights } from "@/lib/api/bodyWeight";
+import {getBodyWeights} from "@/lib/api/bodyWeight";
 
 export default async function BodyWeightPage() {
     const data = await getBodyWeights();
 
     return (
-        <main>
-            <h1>BodyWeights Overview</h1>
-            <ul>
+        <div
+            className="relative overflow-x-auto bg-neutral-primary-soft shadow-xs rounded-base border border-default">
+            <table className="w-full text-sm text-left rtl:text-right text-body">
+                <thead className="bg-neutral-secondary-soft border-b border-default">
+                <tr>
+                    <th scope="col" className="px-6 py-3 font-medium">ID</th>
+                    <th scope="col" className="px-6 py-3 font-medium">Weight</th>
+                    <th scope="col" className="px-6 py-3 font-medium">Date</th>
+                    <th scope="col" className="px-6 py-3 font-medium">Action</th>
+                </tr>
+                </thead>
+                <tbody>
                 {data.map((bodyweight, b) => (
-                    <li key={b}>
-                        {bodyweight.date} — {bodyweight.weight}
-                    </li>
+                    <tr key={b}
+                        className="odd:bg-neutral-primary even:bg-neutral-secondary-soft border-b border-default">
+                        <th scope="row" className="px-6 py-4 font-medium text-heading whitespace-nowrap">
+                            {bodyweight.id}
+                        </th>
+                        <td className="px-6 py-4">{bodyweight.weight}</td>
+                        <td className="px-6 py-4">{bodyweight.date}</td>
+                        <td className="px-6 py-4"><a href="#"
+                                                     className="font-medium text-fg-brand hover:underline">Edit</a>
+                        </td>
+                    </tr>
                 ))}
-            </ul>
-        </main>
+                </tbody>
+            </table>
+        </div>
     );
 }
--- frontend/lib/api/bodyWeight.ts
+++ frontend/lib/api/bodyWeight.ts
@@ -1,6 +1,7 @@
-import { API_BASE_URL } from "./config";
+import {API_BASE_URL} from "./config";
 
 export type BodyWeight = {
+    id: number;
     date: string;
     weight: string;
 };
@@ -10,18 +11,17 @@ export async function getBodyWeights(): Promise<BodyWeight[]> {
         cache: "no-store",
     });
 
-    if (!res.ok) {
-        throw new Error(`Failed to fetch bodyweights: ${res.status}`);
-    }
-
+    if (!res.ok) throw new Error(`Failed to fetch bodyweights: ${res.status}`);
+    
     const data = await res.json();
 
     return data.map((item: BodyWeight) => ({
+        id: item.id,
+        weight: item.weight,
         date: new Date(item.date).toLocaleDateString("nl-NL", {
             day: "2-digit",
             month: "2-digit",
             year: "numeric",
         }),
-        weight: item.weight,
     }));
 }
