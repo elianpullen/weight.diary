58a9a9b9e984cf75c70b27085ff79a71c4d9e8bd
elian <elianpullen@protonmail.com>
2026-07-23 09:20:44 +0200

    chore(frontend): remove date const from bodyweight/edit.tsx

--- frontend/app/bodyweight/[id]/edit/page.tsx
+++ frontend/app/bodyweight/[id]/edit/page.tsx
@@ -3,7 +3,6 @@
 import React, {useEffect, useState} from "react";
 import "react-day-picker/style.css";
 import {getBodyWeight, updateBodyWeight} from "@/lib/api/bodyWeight";
-import DateSelector from "@/app/bodyweight/components/DateSelector";
 
 type Props = {
     params: Promise<{
@@ -12,7 +11,6 @@ type Props = {
 };
 
 export default function EditPage({params}: Props) {
-    const [date, setDate] = useState<Date>();
     const [bodyweight, setBodyweight] = useState("");
     const [loading, setLoading] = useState(true);
 
@@ -20,9 +18,8 @@ export default function EditPage({params}: Props) {
         async function load() {
             try {
                 const {id} = await params;
-                const {date, weight} = await getBodyWeight(Number(id));
+                const {weight} = await getBodyWeight(Number(id));
 
-                setDate(new Date(date));
                 setBodyweight(weight.toString());
             } catch (error) {
                 console.error(error);
@@ -38,15 +35,9 @@ export default function EditPage({params}: Props) {
     async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
         e.preventDefault();
 
-        if (!date) {
-            alert("Select a date.");
-            return;
-        }
-
         const {id} = await params;
 
         await updateBodyWeight(Number(id), {
-            date: date.toISOString().split("T")[0], // year-month-day
             weight: Number(bodyweight),
         });
     }
@@ -60,8 +51,6 @@ export default function EditPage({params}: Props) {
                 onSubmit={handleSubmit}
                 className="flex flex-col gap-6 max-w-sm">
 
-                <DateSelector date={date} setDate={setDate}/>
-
                 <input
                     id="bodyweight"
                     type="number"
@@ -73,7 +62,7 @@ export default function EditPage({params}: Props) {
                     className="w-full rounded border px-3 py-2"
                     required
                 />
-                
+
                 <button
                     type="submit"
                     className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-700">
